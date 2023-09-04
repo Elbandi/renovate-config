69309a5a4943df4abf19ab584a10b02fdfd9a353
justusbunsi <sk.bunsenbrenner@gmail.com>
2023-09-04 19:02:29 +0200

    Configure Renovate runner
    Signed-off-by: justusbunsi <sk.bunsenbrenner@gmail.com>

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "repositories": [
+        "gitea/helm-chart"
+    ],
+    "endpoint": "https://gitea.com/api/v1",
+    "gitAuthor": "Renovate Bot <renovate-bot@gitea.com>",
+    "platform": "gitea",
+    "onboardingConfigFileName": "renovate.json5",
+    "autodiscover": false,
+    "optimizeForDisabled": true,
+    "forkProcessing": "disabled",
+    "dryRun": "full", // null to enable actual PR handling
+    "binarySource": "install",
+    "hostRules": [
+        // {
+        //     "matchHost": "docker.io",
+        //     "username": process.env.HUB_DOCKER_COM_USER,
+        //     "password": process.env.HUB_DOCKER_COM_TOKEN
+        // }
+    ]
+};
